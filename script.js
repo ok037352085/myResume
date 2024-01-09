693eb765eaebe931525763053e4461720cfabcf3
ok037352085 <ok037352085@gmail.com>
2024-01-09 23:44:30 +0800

    reupdate

---
+++ script.js
@@ -0,0 +1,29 @@
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header nav a');
+const menuBtn = document.querySelector('.menu-btn');
+const navigation = document.querySelector('.navigation');
+
+window.onscroll = () => 
+{
+    sections.forEach(sec => 
+        {
+            let top = window.scrollY;
+            let offset = sec.offsetTop - 150;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute('id');
+
+            if(top >= offset && top < offset + height){
+                navLinks.forEach(links => {
+                    links.classList.remove('active');
+                    navigation.classList.remove('active');
+                    menuBtn.classList.remove('active');
+                    document.querySelector('header nav a[href*='+ id +']').classList.add('active');
+                });
+            };
+        });
+};
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('active');
+    navigation.classList.toggle('active');
+});
